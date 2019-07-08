ba75cb65c24bd36c5c1d55bdd378cffdee8d4923
wooddeep <niutourenqz@qq.com>
2019-07-08 23:14:13 +0800

    整理标定

--- app/src/main/java/com/wooddeep/crane/CalibrationSetting.java
+++ app/src/main/java/com/wooddeep/crane/CalibrationSetting.java
@@ -7,6 +7,20 @@ import android.widget.ImageView;
 
 public class CalibrationSetting extends AppCompatActivity {
 
+    // 回转标定
+    private float rotateStartX1 = 0.0f;
+    private float rotateStartY1 = 0.0f;
+    private float rotateStartData = 0.0f; //根据485传输的电压数据
+
+    private float rotateEndX2 = 0.0f;
+    private float rotateEndY2 = 0.0f;
+    private float rotateEndData = 0.0f;   // 根据485传输的电压数据
+
+    private float rotateRate = 0.1f;      // 角度差值 / (rotateEndData - rotateStartData)
+
+    // 档位标定
+    private float Gear1 = 0.1f;  // 一档
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,4 +43,7 @@ public class CalibrationSetting extends AppCompatActivity {
         });
 
     }
+
+
+
 }
