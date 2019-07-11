397aef199d7459e0ceb6dfcd2e081f45b6355288
wooddeep <niutourenqz@qq.com>
2019-07-11 16:44:11 +0800

    -_-

--- app/src/main/java/com/wooddeep/crane/CalibrationSetting.java
+++ app/src/main/java/com/wooddeep/crane/CalibrationSetting.java
@@ -19,7 +19,7 @@ import java.util.List;
 
 public class CalibrationSetting extends AppCompatActivity {
 
-    class CalibrationCell {
+    abstract class CalibrationCell {
         public String dimValueName = null; // 对应的 维度起始值 名称
         public String uartDataName = null; // 对应的 uart起始值 名称
 
@@ -56,53 +56,169 @@ public class CalibrationSetting extends AppCompatActivity {
             this.rateShowId = rsi;
         }
 
-        public void setOnClickListener() {
+        abstract public void setOnClickListener();
+        /*{
             CalibrationCell cell = this;
-            Button btn = findViewById(this.buttonId);
+            Button btn = (Button)findViewById(this.buttonId);
             btn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    TextView tv = findViewById(cell.uartDataTextViewId);
+                    TextView tv = (TextView) findViewById(cell.uartDataTextViewId);
                     if (tv != null) {
-                        tv.setText("xxx"); // TODO 替换为从串口读取数据
+                        //tv.setText("xxx"); // TODO 替换为从串口读取数据
                     }
                 }
             });
-        }
+        }*/
 
     }
 
+    // 回转
+    private CalibrationCell  rotateStartX1 =  new CalibrationCell("rotateStartX1", "rotateStartData", "rotateRate", R.id.etx1, R.id.tv_rotate_coord1, R.id.btn_rotate_coord1, R.id.tv_rotate_rate) {
+        @Override
+        public void setOnClickListener() {
+            //CalibrationCell cell = this;
+            Button btn = (Button)findViewById(rotateStartX1.buttonId);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    float start = 100; // TODO 替换为从串口读取数据
+                    TextView tvStart = (TextView) findViewById(rotateStartX1.uartDataTextViewId);
+                    if (tvStart != null) {
+                        tvStart.setText(String.valueOf(start));
+                    }
+
+                    TextView tvEnd = (TextView) findViewById(rotateEndX2.uartDataTextViewId);
+                    float end = Float.parseFloat(tvEnd.getText().toString());
+
+                    float dataDelta = end - start;
+
+                    System.out.println("#### detaDelata = " + dataDelta);
+                }
+            });
+        }
+    };
+
+    private CalibrationCell  rotateStartY1 =  new CalibrationCell("rotateStartY1", "rotateStartData", "rotateRate", R.id.ety1, R.id.tv_rotate_coord1, R.id.btn_rotate_coord1, R.id.tv_rotate_rate) {
+        @Override
+        public void setOnClickListener() {
+
+        }
+    };
+
+    private CalibrationCell  rotateEndX2 =  new CalibrationCell("rotateEndX2", "rotateEndData", "rotateRate", R.id.etx2, R.id.tv_rotate_coord2, R.id.btn_rotate_coord2, R.id.tv_rotate_rate) {
+        @Override
+        public void setOnClickListener() {
+
+        }
+    };
+
+    private CalibrationCell  rotateEndY2 =  new CalibrationCell("rotateEndY2", "rotateEndData", "rotateRate", R.id.ety2, R.id.tv_rotate_coord2, R.id.btn_rotate_coord2, R.id.tv_rotate_rate) {
+        @Override
+        public void setOnClickListener() {
+
+        }
+    };
+
+    // 档位标定
+    private CalibrationCell  GearRate1 =  new CalibrationCell("GearRate1", -1, -1, R.id.btn_first_gear, R.id.tv_first_gear) {
+        @Override
+        public void setOnClickListener() {
+
+        }
+    };
+
+    private CalibrationCell  GearRate2 =  new CalibrationCell("GearRate2", -1, -1, R.id.btn_gear2, R.id.tv_gear2) {
+        @Override
+        public void setOnClickListener() {
+
+        }
+    };
+
+    private CalibrationCell  GearRate3 =  new CalibrationCell("GearRate3", -1, -1, R.id.btn_gear3, R.id.tv_gear3) {
+        @Override
+        public void setOnClickListener() {
+
+        }
+    };
+
+    private CalibrationCell  GearRate4 =  new CalibrationCell("GearRate4", -1, -1, R.id.btn_gear4, R.id.tv_gear4) {
+        @Override
+        public void setOnClickListener() {
+
+        }
+    };
+
+    private CalibrationCell  GearRate5 =  new CalibrationCell("GearRate5", -1, -1, R.id.btn_gear5, R.id.tv_gear5) {
+        @Override
+        public void setOnClickListener() {
+
+        }
+    };
+
+    // 倾角
+    private CalibrationCell  dipAngleStart =  new CalibrationCell("dipAngleStart", "dipAngleStartData", "dipAngleRate", R.id.et_dip_angle_start, R.id.tv_dip_angle_start, R.id.btn_dip_angle_start, R.id.tv_dip_angle_rate) {
+        @Override
+        public void setOnClickListener() {
+
+        }
+    };
+
+    private CalibrationCell  dipAngleEnd =  new CalibrationCell("dipAngleEnd", "dipAngleEndData", "dipAngleRate", R.id.et_dip_angle_end, R.id.tv_dip_angle_end, R.id.btn_dip_angle_end, R.id.tv_dip_angle_rate) {
+        @Override
+        public void setOnClickListener() {
+
+        }
+    };
+
+    // 重量
+    private CalibrationCell  weightStart =  new CalibrationCell("weightStart", "weightStartData", "weightRate", R.id.et_weight_start, R.id.tv_weight_start, R.id.btn_weight_start, R.id.tv_weight_rate) {
+        @Override
+        public void setOnClickListener() {
+
+        }
+    };
+
+    private CalibrationCell  weightEnd =  new CalibrationCell("weightEnd", "weightEndData", "weightRate", R.id.et_weight_end, R.id.tv_weight_end, R.id.btn_weight_end, R.id.tv_weight_rate) {
+        @Override
+        public void setOnClickListener() {
+
+        }
+    };
+
+    // 长度
+    private CalibrationCell  lengthStart =  new CalibrationCell("lengthStart", "lengthStartData", "lengthRate", R.id.et_arm_length_start, R.id.tv_arm_length_start, R.id.btn_arm_length_start, R.id.tv_arm_length_rate) {
+        @Override
+        public void setOnClickListener() {
+
+        }
+    };
+
+    private CalibrationCell  lengthEnd =  new CalibrationCell("lengthEnd", "lengthEndData", "lengthRate", R.id.et_arm_length_end, R.id.tv_arm_length_end, R.id.btn_arm_length_end, R.id.tv_arm_length_rate) {
+        @Override
+        public void setOnClickListener() {
+
+        }
+    };
+
+    // 高度
+    private CalibrationCell  heightStart =  new CalibrationCell("heightStart", "heightStartData", "heightRate", R.id.et_tower_height_start, R.id.tv_tower_height_start, R.id.btn_tower_height_start, R.id.tv_tower_height_rate) {
+        @Override
+        public void setOnClickListener() {
+
+        }
+    };
+
+    private CalibrationCell  heightEnd =  new CalibrationCell("heightEnd", "heightEndData", "heightRate", R.id.et_tower_height_end, R.id.tv_tower_height_end, R.id.btn_tower_height_end, R.id.tv_tower_height_rate) {
+        @Override
+        public void setOnClickListener() {
+
+        }
+    };
+
+
     private CalibrationCell[] ccs = new CalibrationCell[]{
-
-        // 回转标定
-        new CalibrationCell("rotateStartX1", "rotateStartData", "rotateRate", R.id.etx1, R.id.tv_rotate_coord1, R.id.btn_rotate_coord1, R.id.tv_rotate_rate),
-        new CalibrationCell("rotateStartY1", "rotateStartData", "rotateRate", R.id.ety1, R.id.tv_rotate_coord1, R.id.btn_rotate_coord1, R.id.tv_rotate_rate),
-        new CalibrationCell("rotateEndX2", "rotateEndData", "rotateRate", R.id.etx2, R.id.tv_rotate_coord2, R.id.btn_rotate_coord2, R.id.tv_rotate_rate),
-        new CalibrationCell("rotateEndY2", "rotateEndData", "rotateRate", R.id.ety2, R.id.tv_rotate_coord2, R.id.btn_rotate_coord2, R.id.tv_rotate_rate),
-
-        // 档位标定
-        new CalibrationCell("GearRate1", -1, -1, R.id.btn_first_gear, R.id.tv_first_gear),
-        new CalibrationCell("GearRate2", -1, -1, R.id.btn_gear2, R.id.tv_gear2),
-        new CalibrationCell("GearRate3", -1, -1, R.id.btn_gear3, R.id.tv_gear3),
-        new CalibrationCell("GearRate4", -1, -1, R.id.btn_gear4, R.id.tv_gear4),
-        new CalibrationCell("GearRate5", -1, -1, R.id.btn_gear5, R.id.tv_gear5),
-
-        // 倾角
-        new CalibrationCell("dipAngleStart", "dipAngleStartData", "dipAngleRate", R.id.et_dip_angle_start, R.id.tv_dip_angle_start, R.id.btn_dip_angle_start, R.id.tv_dip_angle_rate),
-        new CalibrationCell("dipAngleEnd", "dipAngleEndData", "dipAngleRate", R.id.et_dip_angle_end, R.id.tv_dip_angle_end, R.id.btn_dip_angle_end, R.id.tv_dip_angle_rate),
-
-        // 重量
-        new CalibrationCell("weightStart", "weightStartData", "weightRate", R.id.et_weight_start, R.id.tv_weight_start, R.id.btn_weight_start, R.id.tv_weight_rate),
-        new CalibrationCell("weightEnd", "weightEndData", "weightRate", R.id.et_weight_end, R.id.tv_weight_end, R.id.btn_weight_end, R.id.tv_weight_rate),
-
-        // 长度
-        new CalibrationCell("lengthStart", "lengthStartData", "lengthRate", R.id.et_arm_length_start, R.id.tv_arm_length_start, R.id.btn_arm_length_start, R.id.tv_arm_length_rate),
-        new CalibrationCell("lengthEnd", "lengthEndData", "lengthRate", R.id.et_arm_length_end, R.id.tv_arm_length_end, R.id.btn_arm_length_end, R.id.tv_arm_length_rate),
-
-        // 高度
-        new CalibrationCell("heightStart", "heightStartData", "heightRate", R.id.et_tower_height_start, R.id.tv_tower_height_start, R.id.btn_tower_height_start, R.id.tv_tower_height_rate),
-        new CalibrationCell("heightEnd", "heightEndData", "heightRate", R.id.et_tower_height_end, R.id.tv_tower_height_end, R.id.btn_tower_height_end, R.id.tv_tower_height_rate),
-
+        rotateStartX1, rotateStartY1, rotateEndX2, rotateEndY2, GearRate1, GearRate2, GearRate3, GearRate4, GearRate5, dipAngleStart, dipAngleEnd, weightStart, weightEnd, lengthStart, lengthEnd, heightStart, heightEnd
     };
 
     @Override
@@ -153,13 +269,13 @@ public class CalibrationSetting extends AppCompatActivity {
                 field.setAccessible(true);
                 Object value = field.get(para);
 
-                EditText et = findViewById(cell.dimValueEditTextId);
+                EditText et = (EditText) findViewById(cell.dimValueEditTextId);
                 if (et != null) {
                     et.setText(value.toString());
                 }
 
                 if (rateName != null) {
-                    TextView tv = findViewById(cell.rateShowId);
+                    TextView tv = (TextView) findViewById(cell.rateShowId);
                     field = Calibration.class.getDeclaredField(rateName);
                     field.setAccessible(true);
                     value = field.get(para);
@@ -167,7 +283,7 @@ public class CalibrationSetting extends AppCompatActivity {
                 }
 
                 if (dataName != null) {
-                    TextView tv = findViewById(cell.uartDataTextViewId);
+                    TextView tv = (TextView) findViewById(cell.uartDataTextViewId);
                     field = Calibration.class.getDeclaredField(dataName);
                     field.setAccessible(true);
                     value = field.get(para);
